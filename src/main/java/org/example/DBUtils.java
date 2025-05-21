6aa98b4ad6a03fdef101c0ac2da599a9733d9039
longqt321 <longqt321@gmail.com>
2025-05-21 22:35:19 +0700

    Final update 2

--- src/main/java/org/example/DBUtils.java
+++ src/main/java/org/example/DBUtils.java
@@ -9,6 +9,7 @@ public class DBUtils {
     private static final String USER = System.getenv("MYSQL_USER");
     private static final String PASSWORD = System.getenv("MYSQL_ROOT_PASSWORD");
 
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
